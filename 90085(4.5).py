48130d8ba62f34819e3a7e9231a3c515c72c5ea9
Arthur1987921 <70871859+Arthur1987921@users.noreply.github.com>
2020-10-06 11:31:57 +0800

    Add files via upload

---
+++ 90085(4.5).py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 10:13:24 2020
+
+@author: cis-user
+"""
+
+# s1='Simon Peter John'
+# s2=s1.split()
+# s3='*'
+# for i in range (0,3):
+#     print(s2[i][0],end='')
+#     print(s3*(len(s2[i])-1),end=',')
+
+# s=['Company 1','Company 2','Company 3']
+# s1=[]
+# for i in range(0,3):
+#     s1.append(s[i].replace(' ','_'))
+# print(s1,end=',')
+
+
+# list2=[1,2,3,4]
+# list1=[5,6,7,8]
+# list3=list(zip(list2,list1))
+# print(list3)
+
+# string=['1','2','3','4','5','6']
+# string1=[]
+# for str in string:
+#     string1.append(str+'$')
+# print(string1)
+
+# s1=['1$', '2$', '3$', '4$', '5$', '6$']
+# s2=[]
+# for i in range (0,6):
+#     s2.append(int(s1[i].replace('$','')))
+# print(s2)
+
+# s = 'I love you and you love him and who loves who'
+# s1=s.split(' ')
+# # print(s1)
+
+# keys=set(s1)
+# values=[0,0,0,0,0,0,0]
+# dict={key: value for key, value in zip(keys,values)}
+# print(dict)
+
+# for i in s1:
+#     dict[i]+=1
+# print(dict)
+
+
+
+
